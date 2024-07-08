8fd923074bbce934f35d7d9464dc5ca5ef9a97ab
tamvt2 <102782719+tamvt2@users.noreply.github.com>
2024-07-08 10:17:15 +0700

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 const userController = require('../app/controllers/UserController');
 
-router.get('/login', userController.showLogin);
-router.get('/register', userController.showRegister);
-router.post('/register', userController.register);
-router.post('/login', userController.login);
-router.get('/logout', userController.logout);
-router.post('/update', userController.update);
+// router.get('/login', userController.showLogin);
+// router.get('/register', userController.showRegister);
+// router.post('/register', userController.register);
+// router.post('/login', userController.login);
+// router.get('/logout', userController.logout);
+// router.post('/update', userController.update);
 router.get('/users', userController.index);
 
 module.exports = router;
